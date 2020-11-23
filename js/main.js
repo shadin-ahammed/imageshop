2935f0781ff7002232b8c6cfc75c2b5988001a68
ITLaptop <WS_172@KCDOMAIN>
2020-11-23 12:03:03 +0600

    frist commit

--- js/main.js
+++ js/main.js
@@ -38,20 +38,20 @@ function w3_open() {
       }
     }
   }
-  var myIndex = 0;
-  carousel();
+  // var myIndex = 0;
+  // carousel();
   
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 9000);    
-  }
+  // function carousel() {
+  //   var i;
+  //   var x = document.getElementsByClassName("mySlides");
+  //   for (i = 0; i < x.length; i++) {
+  //     x[i].style.display = "none";  
+  //   }
+  //   myIndex++;
+  //   if (myIndex > x.length) {myIndex = 1}    
+  //   x[myIndex-1].style.display = "block";  
+  //   setTimeout(carousel, 9000);    
+  // }
   
   //Get the button
   var mybutton = document.getElementById("scrollBtn");
