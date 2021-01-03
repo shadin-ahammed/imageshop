ca7cda88c1e90483ab98c81558704f8702b4ff52
ITLaptop <WS_172@KCDOMAIN>
2021-01-03 15:52:18 +0600

    update

--- js/Contact.js
+++ js/Contact.js
@@ -49,13 +49,13 @@ function checkInputs() {
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
 	const small = formControl.querySelector('small');
-	formControl.className = 'form-control error';
+	formControl.className = 'form-control1 error';
 	small.innerText = message;
 }
 
 function setSuccessFor(input) {
 	const formControl = input.parentElement;
-	formControl.className = 'form-control success';
+	formControl.className = 'form-control1 success';
 }
 	
 function isEmail(email) {
